e36e7c6cca7ef4b0da67c98f193c53a5487aed6c
Ankijit <123397233+Ankijit@users.noreply.github.com>
2023-09-17 15:51:20 +0530

    Add files via upload

---
+++ config/products.mjs
@@ -0,0 +1,380 @@
+export const productsData={
+    "men": [
+      {
+        "id": 1,
+        "name": "Denim jacket",
+        
+        "price": '1000rs',
+        "image": "images/male/1.jpg"
+      },
+      {
+        "id": 2,
+        "name": "Blue shirt",
+        
+        "price": '500rs',
+        "image": "images/male/2.jpg"
+      },
+      {
+        "id": 3,
+        "name": "Blue tshirt",
+        
+        "price": '500rs',
+        "image": "images/male/3.jpg"
+      },
+      {
+        "id": 4,
+        "name": "White turtle neck",
+        
+        "price": '1200rs',
+        "image": "images/male/4.jpg"
+      },
+      {
+        "id": 5,
+        "name": "Men's hoodie",
+        
+        "price": '800rs',
+        "image": "images/male/5.jpg"
+      },
+      {
+        "id": 6,
+        "name": "White t-shirt",
+        
+        "price": '600rs',
+        "image": "images/male/6.jpg"
+      },
+      {
+        "id": 7,
+        "name": "White t-shirt",
+        
+        "price": '600rs',
+        "image": "images/male/7.jpg"
+      },
+      {
+        "id": 8,
+        "name": "Black hoodie",
+        
+        "price": '800rs',
+        "image": "images/male/8.jpg"
+      },
+      {
+        "id": 9,
+        "name": "Jacket",
+        
+        "price": '1200',
+        "image": "images/male/9.jpg"
+      },
+      {
+        "id": 10,
+        "name": "White v-neck",
+        
+        "price": '700rs',
+        "image": "images/male/10.jpg"
+      },
+      {
+        "id": 11,
+        "name": "Suit",
+        
+        "price": '3000rs',
+        "image": "images/male/11.jpg"
+      },
+      {
+        "id": 12,
+        "name": "black neck",
+        
+        "price": '720rs',
+        "image": "images/male/12.jpg"
+      },
+      {
+        "id": 13,
+        "name": "Long jacket",
+        
+        "price": '1100rs',
+        "image": "images/male/13.jpg"
+      },
+      {
+        "id": 14,
+        "name": "Cargo jeans",
+        
+        "price": '1000rs',
+        "image": "images/male/14.jpg"
+      },
+      {
+        "id": 15,
+        "name": "Black jacket",
+        
+        "price": '1099rs',
+        "image": "images/male/15.jpg"
+      },
+      {
+        "id": 16,
+        "name": "Leather jacket",
+        
+        "price": '1300rs',
+        "image": "images/male/16.jpg"
+      }
+    ],
+    "women": [
+      {
+        "id": 17,
+        "name": "Weeding dress blue",
+        
+        "price": '800rs',
+        "image": "images/female/1.jpg"
+      },
+      {
+        "id": 18,
+        "name": "French versate",
+        
+        "price": '1600rs',
+        "image": "images/female/2.jpg"
+      },
+      {
+        "id": 19,
+        "name": "Hoodie combo",
+        
+        "price": '500rs',
+        "image": "images/female/3.jpg"
+      },
+      {
+        "id": 20,
+        "name": "Professional fit",
+        
+        "price": '1400rs',
+        "image": "images/female/4.jpg"
+      },
+      {
+        "id": 21,
+        "name": "Work fit",
+        
+        "price": '700rs',
+        "image": "images/female/5.jpg"
+      },
+      {
+        "id": 22,
+        "name": "Winter jacket",
+        
+        "price": '1100rs',
+        "image": "images/female/6.jpg"
+      },
+      {
+        "id": 23,
+        "name": "Coursette",
+        
+        "price": '800rs',
+        "image": "images/female/7.jpg"
+      },
+      {
+        "id": 24,
+        "name": "Black tshirt",
+        
+        "price": '500rs',
+        "image": "images/female/8.jpg"
+      },
+      {
+        "id": 25,
+        "name": "Idk dress",
+        
+        "price": '500rs',
+        "image": "images/female/9.jpg"
+      },
+      {
+        "id": 26,
+        "name": "Summer combo",
+        
+        "price": '1000rs',
+        "image": "images/female/10.jpg"
+      },
+      {
+        "id": 27,
+        "name": "Pink jacket",
+        
+        "price": '1000rs',
+        "image": "images/female/11.jpg"
+      },
+      {
+        "id": 28,
+        "name": "Rockstar combo",
+        
+        "price": '900rs',
+        "image": "images/female/12.jpg"
+      },
+      {
+        "id": 29,
+        "name": "White shirt",
+        
+        "price": '600rs',
+        "image": "images/female/13.jpg"
+      },
+      {
+        "id": 30,
+        "name": "The date look",
+        
+        "price":'1600rs',
+        "image": "images/female/14.jpg"
+      },
+      {
+        "id": 31,
+        "name": "Winter look",
+        
+        "price": '700rs',
+        "image": "images/female/15.jpg"
+      },
+      {
+        "id": 32,
+        "name": "Blue denim",
+        
+        "price": '1200rs',
+        "image": "images/female/16.jpg"
+      }
+    ],
+    
+    "athletes": [
+      {
+        "id": 33,
+        "name": "The gym look",
+        
+        "price": '800rs',
+        "image": "images/althletes/1.jpg"
+      },
+      {
+        "id": 34,
+        "name": "Calvin klein combo",
+        
+        "price": '1700rs',
+        "image": "images/althletes/2.jpg"
+      },
+      {
+        "id": 35,
+        "name": "Sweatproof tshirt",
+        
+        "price": '900rs',
+        "image": "images/althletes/3.jpg"
+      },
+      {
+        "id": 36,
+        "name": "Tennis combo",
+        
+        "price": '1000rs',
+        "image": "images/althletes/4.jpg"
+      },
+      {
+        "id": 37,
+        "name": "Crop top",
+        
+        "price": '800rs',
+        "image": "images/althletes/5.jpg"
+      },
+      {
+        "id": 38,
+        "name": "Men's innerwear",
+        
+        "price": '200rs',
+        "image": "images/althletes/6.jpg"
+      },
+      {
+        "id": 39,
+        "name": "Men's gym combo",
+        
+        "price": '1000rs',
+        "image": "images/althletes/7.jpg"
+      },
+      {
+        "id": 40,
+        "name": "Athletic combo",
+        
+        "price": '900rs',
+        "image": "images/althletes/8.jpg"
+      },
+      {
+        "id": 41,
+        "name": "Tennis season",
+        
+        "price": '1200rs',
+        "image": "images/althletes/9.jpg"
+      },
+      {
+        "id": 42,
+        "name": "Shorts combo",
+        
+        "price": '800rs',
+        "image": "images/althletes/10.jpg"
+      },
+      {
+        "id": 43,
+        "name": "Basketball season",
+        
+        "price": '500rs',
+        "image": "images/althletes/11.jpg"
+      },
+      {
+        "id": 44,
+        "name": "Bodybuilder wear",
+        
+        "price": '1000rs',
+        "image": "images/althletes/12.jpg"
+      },
+      {
+        "id": 45,
+        "name": "White top",
+        
+        "price": '500rs',
+        "image": "images/althletes/13.jpg"
+      },
+      {
+        "id": 46,
+        "name": "White combo",
+        
+        "price": '1000rs',
+        "image": "images/althletes/14.jpg"
+      },
+      {
+        "id": 47,
+        "name": "Pushup bra",
+        
+        "price": '300rs',
+        "image": "images/althletes/15.jpg"
+      },
+      {
+        "id": 48,
+        "name": "White wear",
+        
+        "price": '600rs',
+        "image": "images/althletes/16.jpg"
+      },
+      {
+        "id": 49,
+        "name": "Gym wear",
+        
+        "price": '800rs',
+        "image": "images/althletes/17.jpg"
+      },
+      {
+        "id": 50,
+        "name": "Stripe tshirt",
+        
+        "price": '600rs',
+        "image": "images/althletes/18.jpg"
+      },
+      {
+        "id": 51,
+        "name": "Cargo wear",
+        
+        "price": '600rs',
+        "image": "images/althletes/19.jpg"
+      },
+      {
+        "id": 52,
+        "name": "Innerwear combo",
+        
+        "price": '500rs',
+        "image": "images/althletes/20.jpg"
+      }
+    ]
+  }
+  
+
+  export const allProducts = [].concat(
+    productsData.men,
+    productsData.women,
+    productsData.athletes
+  );
